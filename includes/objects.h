cfdde1248766f292e9d10a024dfa1e8b5c4778ac
LESdylan <dev.pro.photo@gmail.com>
2025-12-29 11:37:15 +0100

    started the work of objects shapes... algorithm

---
+++ includes/objects.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   objects.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/29 11:34:49 by marvin            #+#    #+#             */
+/*   Updated: 2025/12/29 11:34:49 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef OBJECTS_H
+# define OBJECTS_H
+
+# include "../srcs/objects/box.h"
+# include "../srcs/objects/composite.h"
+# include "../srcs/objects/grid.h"
+# include "../srcs/objects/instance.h"
+# include "../srcs/objects/plane.h"
+# include "../srcs/objects/rectangle.h"
+# include "../srcs/objects/smooth_triangle.h"
+# include "../srcs/objects/sphere.h"
+# include "../srcs/objects/triangle.h"
+
+#endif
