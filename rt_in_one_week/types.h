0e8ce79c013d24db81e44716c01494780df525e9
LESdylan <dev.pro.photo@gmail.com>
2026-01-01 17:23:27 +0100

    added linear interpolatin logic

--- rt_in_one_week/types.h
+++ rt_in_one_week/types.h
@@ -6,12 +6,26 @@
 /*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/01 16:47:22 by dlesieur          #+#    #+#             */
-/*   Updated: 2026/01/01 16:48:19 by dlesieur         ###   ########.fr       */
+/*   Updated: 2026/01/01 17:12:22 by dlesieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPES_H
 # define TYPES_H
+
+# ifndef FLOAT_TYPE
+typedef struct s_point3
+{
+	double	e[3];
+}	t_point3;
+# else
+
+typedef struct s_point3
+{
+	float	e[3];
+}	t_point3;
+
+#endif
 
 #ifndef FLOAT_TYPE
 typedef double real_t;
